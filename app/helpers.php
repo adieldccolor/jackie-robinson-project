a401f13be52e8be79b48b648a75b14392e8fc65e
adielhercules <adiel@dccolorweb.com>
2015-03-05 23:28:24 -0600

    compilation functionality improvements

--- app/helpers.php
+++ app/helpers.php
@@ -157,7 +157,7 @@ function compile()
 		$posted = true;
 		$file = $_REQUEST['file'];
 	}else{
-		echo 'Compilando.<script>document.getElementsByTagName(\'form\')[0].submit();</script>';
+		echo '<script>document.getElementsByTagName(\'form\')[0].submit();</script>';
 	}
 
 	if( $posted )
@@ -217,13 +217,15 @@ function xcopy($source, $dest, $permissions = 0755)
 
 
 
+
+
 function createFile($file="", $uri="")
 {
+
 
 	if( !is_dir("compile/") ){
 		mkdir("compile");
 	}
-
 
 	if( !is_dir("compile/assets/") ){
 		mkdir("compile/assets/");
@@ -244,17 +246,79 @@ function createFile($file="", $uri="")
 	fwrite($myfile, $txt);
 	fclose($myfile);
 	$uri_redir = $uri == "index" ? "?" : $uri;
-	return "<span style='display:block;padding: 20px;background: #71ADAF; color: #fff;'>File '$uri' created. 
+	return "<span style='display:block;padding: 20px;background: #71ADAF; color: #fff;
+	 position: fixed; bottom: 10px; left: 10px; right: 10px;'>
+		File '$uri' created. 
 			Redirecting...</span>
 			<script>setTimeout(function(){window.location='$uri_redir';},2000);</script>";
 }
 
 
+function open_compile_template(){
+	return '<html><head>
+<title>Compilacion en curso</title>        
+<link href=\'//fonts.googleapis.com/css?family=Lato:100,400,700\' rel=\'stylesheet\' type=\'text/css\'>
+<style>
+body { margin: 0; padding: 0; width: 100%; height: 100%; color: #B0BEC5; 
+	display: table; font-weight: 100; font-family: \'Lato\'; }
+.container { text-align: center; display: table-cell; vertical-align: middle; }
+.content { text-align: center; display: inline-block; }
+.title { font-size: 50px; margin-bottom: 40px; }
+.quote { font-size: 24px; }
+</style>
+</head><body>
+<div class="container">
+    <div class="content">
+        <div class="title">Compilando</div>
+        <div class="quote">Espere mientras su archivo es compilado.</div>
+<form method="post"><textarea rows="10" name="file" style="position: absolute; opacity: 0;visibility: hidden;">';
+}
+
+function close_compile_template(){
+	return '</textarea data-noreplace><div class="btns" style="padding-top: 20px">
+<input type="submit" value="Compilar pagina" style="position:fixed; bottom: 20px; 
+left: 20px; padding: 20px; border: none; color: #fff; font-size: 18px; 
+background: #94B933; z-index: 999999999999; cursor: pointer;opacity:0;visibility:hidden;"></div></form></div>';
+}
+
+
+function end_compile_template(){
+	return '</div></body></html>';
+}
 
 
 
 
+function compile_snackbar(){
+	$buttons = "";
+	$buttonstyle = 'position:fixed; bottom: 5px; 
+				left: 5px; padding: 0px 20px; border: none; color: #fff; 
+				background: #94B933; z-index: 999999999999; width: 175px; 
+				height: 50px; line-height: 50px; box-shadow: 0px 1px 5px rgba(0,0,0,0.2);
+				font-size: 10pt; font-family: sans-serif; text-transform: uppercase; opacity: 0.6;
+				transition: all 0.3s';
 
+	$buttons .= "<a href=\"?compile\" style=\"$buttonstyle\" 
+					onmouseenter=\"this.style.opacity=1\" 
+					onmouseleave=\"this.style.opacity=0.6\"
+					>Compilar pagina</a>";
+
+    $uri = Route::uri();
+    $uri = $uri == "" || $uri == "/" ? "index" : $uri;
+    if( file_exists("compile/" . $uri . ".html") ){
+    	$buttons .= "<a href=\"compile/$uri.html\" style=\"$buttonstyle; bottom: 70px\"
+    					onmouseenter=\"this.style.opacity=1\" 
+						onmouseleave=\"this.style.opacity=0.6\"
+    					>Ver archivo</a>";
+    }else{
+    	$buttons .= "<span style=\"position:fixed;bottom:60px;left:5px; opacity: 0.6; background: #000;
+    	color: #fff; padding: 5px; border-radius: 4px; z-index: 999999999999;  font-size: 10pt;
+    	\">Pagina aun no compilada.</span>";
+    }
+
+    return $buttons;
+
+}
 
 
 
